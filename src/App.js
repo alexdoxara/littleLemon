a6f33df43af87915a48d865387f031043b1768a7
Alex <a.papoutsis96@gmail.com>
2023-10-19 19:29:50 +0200

    added new files to structure the page with scemanti Html tags

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import './Nav'
+import Nav from './Nav';
+import Main from './Main';
+import Section1 from './Section1';
+import Section2 from './Section2';
+import Section3 from './Section3';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <Nav />
+      <Main />
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Footer />
+    </>
   );
 }
 
