48d56e6ded8d524aa4a1b5f17ee0af1063c26d2b
Alex <a.papoutsis96@gmail.com>
2023-10-20 00:14:24 +0200

    Added a logo and some text to the navbar and the footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,6 +1,29 @@
 const Footer = () => {
     return (
-        <p>Hello from Footer</p>
+        <footer>
+            <img />
+            <ul>
+                <li>Doorma Navigation</li>
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Reservations</li>
+                <li>Order Online</li>
+                <li>Login</li>
+            </ul>
+            <ul>
+                <li>Contact</li>
+                <li>Address</li>
+                <li>Phone number</li>
+                <li>E-mail</li>
+            </ul>
+            <ul>
+                <li>Contact</li>
+                <li>Address</li>
+                <li>Phone number</li>
+                <li>E-mail</li>
+            </ul>
+        </footer>
     )
 }
 
--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,19 @@
+import logo from "./assets/Logo.svg"
+
 export default function Nav() {
     return (
-        <h1>Hello World</h1>
+        <nav>
+
+            <img src={logo} />
+            <ul>
+                <li><a>Home</a></li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Reservations</li>
+                <li>Order Online</li>
+                <li>Log in</li>
+            </ul>
+        </nav>
+
     )
 }
